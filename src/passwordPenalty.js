46bd4b1f026e0b94a3b278d55e3e1fdc6c7ec92d
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-16 12:27:29 +0200

    fix: Added the explanation on the regex in the file passwordPenalty.jsx

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -8,6 +8,9 @@
 export default function penaltyPoints(password = "") {
   if (!password) return 0;
 
+  /*
+  The following line contain a regex that look for any character that repeat it self as a group. 
+  */
   const SequenceNumber = password?.match(/(.)\1+/gm);
 
   let penalities = 0;
