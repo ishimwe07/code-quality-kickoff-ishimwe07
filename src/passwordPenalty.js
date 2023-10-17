e4450089df5b34bdf56e591658e86beb8294f7c2
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-17 12:04:37 +0200

    Fix: Done refactoring the codes in password validation file to ensure the quality.

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,19 +1,13 @@
-export default function penaltyPoints(password = "") {
+export default function penaltyPoints(password) {
   if (!password) return 0;
-
-  /*
-  The following line contain a regex that look for any character that repeat it self as a group. 
-  */
-  const SequenceNumber = password?.match(/(.)\1+/gm);
 
   let penalities = 0;
 
-  SequenceNumber?.forEach((element) => {
-    if (element.length > 2) {
-      penalities += 2;
-    } else {
-      penalities++;
-    }
-  });
+  // The following line contain a regex that look for any character that repeat it self as a group.
+  password.match(/(.)\1+/gm)
+    ?.forEach((element) =>
+      element.length > 2 ? (penalities += 2) : penalities++
+    );
+
   return penalities;
 }
