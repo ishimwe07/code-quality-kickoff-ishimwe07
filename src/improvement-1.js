8fd632495dd3b058bd7b50332e5f9f9d71decffe
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-18 11:09:25 +0200

    Fix: Refactoring the codes styles using prettier and removing unnecessary comments.

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -1,10 +1,3 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 1
  const countUpperCaseRs = letters => letters.match(/R/g).length;
 
 export default countUpperCaseRs;
--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -4,7 +4,8 @@ export default function penaltyPoints(password) {
   let penalities = 0;
 
   // The following line contain a regex that look for any character that repeat it self as a group.
-  password.match(/(.)\1+/gm)
+  password
+    .match(/(.)\1+/gm)
     ?.forEach((element) =>
       element.length > 2 ? (penalities += 2) : penalities++
     );
