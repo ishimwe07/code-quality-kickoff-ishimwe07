93cab08661730a1410dc1ba2ebb91f5b4deb1715
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-26 11:56:26 +0200

    refactor: Naming parameters more descriptive names

--- src/Exercise 5/fibonacciSequence.js
+++ src/Exercise 5/fibonacciSequence.js
@@ -1,6 +1,6 @@
-const fibonacciSequence = n => {
-  const sequence = [0,1, 1];
-  for (let i = 1; i < n - 1; i++) {
+const fibonacciSequence = iterationNumber => {
+  const sequence = [0, 1, 1];
+  for (let i = 1; i < iterationNumber - 1; i++) {
     sequence.push(sequence[i] + sequence[i + 1]);
   }
   return sequence;
